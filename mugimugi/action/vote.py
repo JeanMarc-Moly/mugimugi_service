268c069c5840c7f959a659d9223b2d7f604fb8da
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2020-10-06 21:27:21 +0200

    fix vote

--- mugimugi/action/vote.py
+++ mugimugi/action/vote.py
@@ -1,20 +1,33 @@
+from typing import Union, Iterable
 from fast_enum import FastEnum
 
-from ..enum import Action, Score
+from ..enum import Action, Score, ItemType
 from .abstract import AbstractAction
 
 
 class Parameter(metaclass=FastEnum):
-    SCORE = "score"
+    ID = "ID"  # ItemType + int
+    SCORE = "score"  # Score
 
 
 class Vote(AbstractAction):
-    def __init__(self, score: str):
-        super().__init__(Action.VOTE)
+    IDS_SEPARATOR = ","
+
+    def __init__(self, ids: Iterable[tuple[int, Union[str, ItemType]]], score: str):
+        self.ids = ids = {(id_, ItemType[type_]) for id_, type_ in ids}
+        if not ids:
+            raise Exception("Require at least one id")
         self.score = Score[score]
 
+    @staticmethod
+    def get_action() -> Action:
+        return Action.VOTE
+
     @property
-    def params(self):
+    def params(self) -> dict[str, Union[int, str]]:
         params = super().params
+        params[Parameter.ID.value] = self.IDS_SEPARATOR.join(
+            type_.value + str(id_) for id_, type_ in self.ids
+        )
         params[Parameter.SCORE.value] = self.score.value
         return params
