ebe82a290b0df26f7ab485785dc7ee794b2d123d
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-01-16 15:53:37 +0100

    fix convention search by date

--- mugimugi/service/convention.py
+++ mugimugi/service/convention.py
@@ -1,5 +1,6 @@
+from contextlib import suppress
 from datetime import date
-from typing import AsyncIterator, ClassVar, Optional, Type
+from typing import AsyncIterator, ClassVar, Coroutine, Iterator, Optional, Type
 
 from ..action import SearchItem
 from ..entity.item.convention import ConventionRoot
@@ -20,9 +21,30 @@ class Convention(Item[ConventionRoot]):
         contributor: Optional[str] = None,
         sort_criterion: Optional[SearchItem.SortCriterion] = None,
         sort_order: Optional[SortOrder] = None,
-        limit: Optional[int] = None,
+        limit: Optional[int] = 0,
     ) -> AsyncIterator[ConventionRoot]:
-        async for item in self.fetch_all_elements(
+        with suppress(StopIteration):
+            parse = self.CONSTRUCTOR.parse
+            pages = self.search_pages(
+                title, date_, contributor, sort_criterion, sort_order
+            )
+            page = None
+            while page := pages.send(page):
+                page = parse(await page)
+                for element in page.elements:
+                    yield element
+                    if not (limit := limit - 1):
+                        return
+
+    def search_pages(
+        self,
+        title: Optional[str] = None,
+        date_: Optional[date] = None,
+        contributor: Optional[str] = None,
+        sort_criterion: Optional[SearchItem.SortCriterion] = None,
+        sort_order: Optional[SortOrder] = None,
+    ) -> Iterator[Coroutine]:
+        action = iter(
             SearchItem(
                 self.SEARCH_TYPE,
                 title,
@@ -30,7 +52,10 @@ class Convention(Item[ConventionRoot]):
                 contributor=contributor,
                 sort_criterion=sort_criterion,
                 sort_order=sort_order,
-            ),
-            limit=limit,
-        ):
-            yield item
+            )
+        )
+        query = self._api.query
+        response = None
+        with suppress(StopIteration):
+            while paginated_action := action.send(response):
+                response = yield query(paginated_action)
