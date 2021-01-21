f904a09aae6c8b0956b911754af66dc37b7b7015
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-01-21 23:14:45 +0100

    book parsing test

---
+++ tests/sample/book/item/content/test_elf.py
@@ -0,0 +1,43 @@
+from unittest.case import TestCase
+
+from mugimugi.entity.book import Linker
+from mugimugi.entity.item import LinkedPartialContent
+from mugimugi.enum import Ratio
+
+from .....configuration import SAMPLE
+from ....sample import Sample
+
+
+class TestBookContentElf(Sample, TestCase):
+    file_path = SAMPLE / "book/item/content/elf.xml"
+    type = LinkedPartialContent
+    object = LinkedPartialContent(
+        english_name="Elf",
+        japanese_name="エルフ",
+        romaji_name="",
+        other_names=[],
+        id="K39",
+        version=1,
+        objects_count=2803,
+        ratio=Ratio.NOT_SET,
+    )
+
+
+class TestBookContentElfFromUnion(Sample):
+
+    file_path = SAMPLE / "book/item/elf.xml"
+    type = Linker
+    object = Linker(
+        [
+            LinkedPartialContent(
+                english_name="Elf",
+                japanese_name="エルフ",
+                romaji_name="",
+                other_names=[],
+                id="K39",
+                version=1,
+                objects_count=2803,
+                ratio=Ratio.NOT_SET,
+            )
+        ]
+    )
---
+++ tests/sample/sample.py
@@ -0,0 +1,22 @@
+from dataclasses import asdict
+from pathlib import Path
+from typing import Type
+
+from mugimugi.entity.root import parse
+
+from ..configuration import SAMPLE
+
+
+class Sample:
+    file_path: Path
+    object: object
+    type: Type
+
+    def test_parsing(self):
+        self.maxDiff = None
+        with (path := self.file_path).open("r") as f:
+            self.assertDictEqual(
+                asdict(parse(self.type, f.read())),
+                asdict(self.object),
+                f"Failed to properly parse {path.relative_to(SAMPLE)}",
+            )
