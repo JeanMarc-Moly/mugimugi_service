a481436b22858d087d6b51f3ed267dcf134c5f4e
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-03-13 16:12:25 +0100

    additional item UT

---
+++ tests/resources/xml/sample/item/publisher/no.py
@@ -0,0 +1,18 @@
+from mugimugi.entity.main.book import Publisher
+
+from .......configuration import SAMPLE
+from ....sample import Sample
+
+
+class BookPublisherNo(Sample):
+    file_path = SAMPLE / "book/item/publisher/no.xml"
+    type = Publisher
+    object = Publisher(
+        english_name="No Publisher",
+        japanese_name="出版社なし",
+        romaji_name="",
+        other_names=[],
+        _id="L3",
+        version=2,
+        objects_count=1492741,
+    )
