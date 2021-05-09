646e5eac65994c1a1150dbcc2387a534db7fc1a5
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-05-09 16:15:39 +0200

    harmonize paginated with bulk

--- mugimugi/action/abstract_paginated.py
+++ mugimugi/action/abstract_paginated.py
@@ -2,13 +2,15 @@ from abc import ABC
 from enum import Enum
 from typing import Iterator, Optional, Union
 
-from ..configuration import RESPONSE_MAX_COUNT
+from ..configuration import PARALLEL_PAGES_COUNT, RESPONSE_MAX_COUNT
 from .abstract import AbstractAction, AsyncClient
 
 
 class AbstractPaginatedAction(AbstractAction, ABC):
     class Parameter(Enum):
         PAGE = "page"  # int > 0
+
+    PAGES: int = PARALLEL_PAGES_COUNT
 
     page: Optional[int] = None
 
@@ -20,13 +22,7 @@ class AbstractPaginatedAction(AbstractAction, ABC):
         yield from super().params()
         yield self.Parameter.PAGE.value, self.page
 
-    def query_many(self, client: AsyncClient, blind: bool = True):
-        if blind:
-            self.query_many_blind(client)
-        else:
-            self.query_many_smart(client)
-
-    async def query_many_blind(self, client: AsyncClient):
+    async def query_elements_fast(self, client: AsyncClient):
         current, swap = 0, self.page
 
         while True:
@@ -37,15 +33,16 @@ class AbstractPaginatedAction(AbstractAction, ABC):
             result = self.send_and_parse(query)
             yield result
 
-    async def query_many_smart(self, client: AsyncClient):
+    async def query_elements(self, client: AsyncClient):
         current, swap = 0, self.page
         count = max_ = self.page_size
 
         while count == max_:
             self.page = current = current + 1
-            query = self.get_query(client)
+            result = self.send_and_parse(client, self.get_query(client))
             self.page = swap
 
-            result = self.send_and_parse(client, query)
-            yield result
-            count = len((await result).elements)
+            result = (await result).elements
+            for e in result:
+                yield e
+            count = len(result)
